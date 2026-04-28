15a2095c2910d77e768795509c7a2a1657bc0658
AKASH DAS <akashcdas027@gmail.com>
2026-04-28 21:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.50%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c : s) {
+            // If opening bracket → push
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            else {
+                // If stack empty → invalid
+                if(st.empty()) return false;
+
+                char top = st.top();
+
+                // Check matching
+                if((c == ')' && top == '(') ||
+                   (c == '}' && top == '{') ||
+                   (c == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // If stack empty → valid
+        return st.empty(); 
+    }
+};
